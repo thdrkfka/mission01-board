220aacbfa091b089f20c2b2e1f4b139a77fe23f6
thdrkfka <ssongjiaran@gmail.com>
2024-02-19 12:32:19 +0900

    [Feat] : Post Entity 생성
    세부사항
    - postman 사용해서 데이터가 데이터베이스에 들어가는지 확인

--- src/main/java/org/ohgiraffers/board/controller/PostController.java
+++ src/main/java/org/ohgiraffers/board/controller/PostController.java
@@ -10,7 +10,6 @@ import org.ohgiraffers.board.domain.dto.CreatePostRequest;
 import org.ohgiraffers.board.domain.dto.CreatePostResponse;
 import org.ohgiraffers.board.service.PostService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
